dcc014ededfa17501e3613d397c7d98866d2bd5f
Cain Atkinson <yellowsink@protonmail.com>
2021-10-24 20:48:25 +0100

    surely im not that dumb

--- src/ThemeToggler.jsx
+++ src/ThemeToggler.jsx
@@ -3,7 +3,7 @@ import light from "./assets/light.svg";
 
 export default () => (
   <div class="flex mb-10 self-center">
-    <img src={dark} class="dark:(filter invert)" />
+    <img src={light} class="dark:(filter invert)" />
 
     <button
       class="w-12 h-6 rounded-full bg-dark-800 dark:bg-dark-50 mx-2 flex items-center"
@@ -16,6 +16,6 @@ export default () => (
       <div class="w-4 h-4 rounded-full bg-light-500 mx-1 dark:ml-7 transition-all" />
     </button>
 
-    <img src={light} class="dark:(filter invert)" />
+    <img src={dark} class="dark:(filter invert)" />
   </div>
 );
