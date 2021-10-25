c63f1e065760256ca53eedf0e0cf40ac0ab30de2
Cain Atkinson <yellowsink@protonmail.com>
2021-10-25 20:00:58 +0100

    there thats the cards

--- src/components/AccountCard.jsx
+++ src/components/AccountCard.jsx
@@ -2,11 +2,11 @@ export default ({ banner, label, link, uname }) => (
   <a
     href={link}
     target={"_blank"}
-    class="border border-gray-500 shadow-md p-4 w-70 h-30 <md:w-auto flex justify-between"
+    class="border border-gray-500 shadow-md p-8 w-80 h-40 <md:w-auto flex justify-between bg-light-300 dark:(bg-dark-500 border-gray-400) transition-colors"
   >
-    <img class="-m-4 mr-4 h-20 h-auto object-cover" src={banner} />
+    <img class="-m-2 mr-4 h-20 h-auto object-cover" src={banner} />
     <div class="text-right self-center">
-      <h2 class="text-lg">{label}</h2>
+      <h2 class="text-2xl">{label}</h2>
       <p>{uname}</p>
     </div>
   </a>
--- src/routes/Accounts.jsx
+++ src/routes/Accounts.jsx
@@ -1,41 +1,53 @@
 import AccountCard from "../components/AccountCard";
+import osu from "../assets/accounticons/osu.png";
+import reddit from "../assets/accounticons/reddit.png";
+import discord from "../assets/accounticons/discord.png";
+import twitter from "../assets/accounticons/twitter.png";
+import youtube from "../assets/accounticons/youtube.png";
 
 const accounts = [
   {
-    banner: "",
-    label: "727",
+    banner: osu,
+    label: "osu!",
     link: "https://osu.ppy.sh/u/yellowsink",
-    uname: "Yellowsink"
-  }, {
-    banner: "",
-    label: "727",
-    link: "https://osu.ppy.sh/u/yellowsink",
-    uname: "Yellowsink"
-  }, {
-    banner: "",
-    label: "727",
-    link: "https://osu.ppy.sh/u/yellowsink",
-    uname: "Yellowsink"
-  }, {
-    banner: "",
-    label: "727",
-    link: "https://osu.ppy.sh/u/yellowsink",
-    uname: "Yellowsink"
-  }, {
-    banner: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Osu%21Logo_%282015%29.svg/1024px-Osu%21Logo_%282015%29.svg.png",
-    label: "727",
-    link: "https://osu.ppy.sh/u/yellowsink",
-    uname: "Yellowsink"
+    uname: "Yellowsink",
   },
-]
+  {
+    banner: discord,
+    label: "Discord",
+    uname: "Yellowsink#8156",
+  },
+  {
+    banner: reddit,
+    label: "Reddit",
+    link: "https://reddit.com/u/yellosink",
+    uname: "u/yellosink",
+  },
+  {
+    banner: twitter,
+    label: "Twitter",
+    link: "https://twitter.com/CainAtkin",
+    uname: "@CainAtkin",
+  },
+  {
+    banner: youtube,
+    label: "YouTube",
+    link: "https://www.youtube.com/channel/UCQ_ZGydjNeSHNbdo_m6uUcQ",
+    uname: "Cain Atkinson",
+  },
+];
 
 export default () => (
   <>
-    <h2 class="text-xl">Accounts</h2>
-    <div class="text-4xl mt-6">Under construction 🏗</div>
+    <h2 class="text-3xl">Accounts</h2>
     <div class="flex flex-wrap <md:grid grid-cols-2 <sm:grid-cols-1 justify-evenly gap-6 my-4">
-      {accounts.map(acc => (
-        <AccountCard banner={acc.banner} label={acc.label} link={acc.link} uname={acc.uname} />
+      {accounts.map((acc) => (
+        <AccountCard
+          banner={acc.banner}
+          label={acc.label}
+          link={acc.link}
+          uname={acc.uname}
+        />
       ))}
     </div>
   </>
