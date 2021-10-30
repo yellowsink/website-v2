7387c52b2f02d980c8e1d86c7496ead44a80e961
Cain Atkinson <yellowsink@protonmail.com>
2021-10-30 11:54:26 +0100

    move to solidown :rocket:

--- src/routes/Plugins.jsx
+++ src/routes/Plugins.jsx
@@ -1,28 +1,14 @@
-import { createResource, createSignal } from "solid-js";
-import marked from "marked";
 import markdowncss from "../assets/markdown.css";
+import Solidown from "solidown"
 
-const sourceUrl =
-  "https://raw.githubusercontent.com/yellowsink/cc-plugins/master/README.md";
-
-const getMarkdownSource = async () => await (await fetch(sourceUrl)).text();
-
-export default () => {
-  const [fetchSignal, setFetchSignal] = createSignal();
-  const [markdown] = createResource(fetchSignal, getMarkdownSource);
-  setFetchSignal(0);
-
-  return (
-    <>
-      {markdown() ? (
-        <div innerHTML={marked(markdown())} id="markdown-container" />
-      ) : (
-        <div class="text-center mt-20 text-6xl font-mono font-thin">
-          Loading MD...
-        </div>
-      )}
-
-      <style>{markdowncss}</style>
-    </>
-  );
-};
+export default () => (
+  <Solidown
+    src="https://raw.githubusercontent.com/yellowsink/cc-plugins/master/README.md"
+    css={markdowncss}
+    loading={
+      <div class="text-center mt-20 text-6xl font-mono font-thin">
+        Loading MD...
+      </div>
+    }
+  />
+);
