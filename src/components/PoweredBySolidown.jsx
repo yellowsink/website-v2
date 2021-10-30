f932919da21fcf41e68a35f8ff57a3648d5166eb
Cain Atkinson <yellowsink@protonmail.com>
2021-10-30 16:26:28 +0100

    thats a bit cleaner

--- src/components/PoweredBySolidown.jsx
+++ src/components/PoweredBySolidown.jsx
@@ -1,9 +1,9 @@
 import { Link } from "solid-app-router";
 
 export default () => (
-  <>
+  <div class="flex flex-col">
     <div
-      class="font-mono italic float-right pl-4 pt-2 mt-6 border border-current border-b-transparent border-r-transparent transition-colors"
+      class="font-mono italic pl-4 pt-2 mt-6 border border-current border-b-transparent border-r-transparent transition-color w-max ml-auto"
       style={{ "border-top-left-radius": "1rem" }}
     >
       Rendered by{" "}
@@ -11,9 +11,9 @@ export default () => (
         Solidown
       </Link>
     </div>
-    <div class="italic text-right float-right mt-2">
+    <div class="italic text-right mt-2">
       Please note that this label is not rendered by, nor part of Solidown, but there to
       inform that these specific pages are indeed rendered by Solidown
     </div>
-  </>
+  </div>
 );
