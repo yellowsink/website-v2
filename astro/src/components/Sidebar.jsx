9ec2339dfbb31fead935cfd139b9412c8c15305f
Cain Atkinson <yellowsink@protonmail.com>
2022-01-09 23:46:09 +0000

    yes

--- astro/src/components/Sidebar.jsx
+++ astro/src/components/Sidebar.jsx
@@ -3,15 +3,13 @@ export default ({ routes, currentPath }) => {
     <div id="sidebar">
       {/* <ThemeToggler /> */}
 
-      <h1 class="text-4xl text-center font-thin">sink's place</h1>
-      <h2 class="font-bold mb-4 mt-10 text-xl">Pages:</h2>
-      <ul class="text-lg italic mb-10">
+      <h1>sink's place</h1>
+      <h2>Pages:</h2>
+      <ul>
         {routes.map((route) => (
           <li>
             <a
-              class={`text-black dark:text-white no-underline hover:pl-4 transition-all ${
-                route.path === currentPath ? "active" : ""
-              }`}
+              class={route.path === currentPath ? "active" : ""}
               href={route.path}
             >
               {route.name}
@@ -22,12 +20,7 @@ export default ({ routes, currentPath }) => {
 
       <div style="flex: 1" />
 
-      <p id="sbar_bottomtext" class="-m-14 mt-0 text-right text-sm">
-        Made with Astro, Solid, and VSCode
-      </p>
-
-      {/* forgive me for css but solid app router go brr */}
-      <style>{".active { margin-left: 2rem; }"}</style>
+      <p id="sbar_bottomtext">Made with Astro, Solid, and VSCode</p>
     </div>
   );
 };
